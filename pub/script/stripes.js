88f296f0ab06427ae8d931b58dec01be4aaabf29
Slippy Douglas <git@slippyd.com>
2011-02-23 05:08:07 -0800

    Transition animation fix for Firefox (doesn't like transitioning just the “…-y” part of “background-position”).
    • Sped it up, too.

--- pub/script/stripes.js
+++ pub/script/stripes.js
@@ -4,12 +4,12 @@ $(document).onReady(function() {
 	var stripesLayer = $$('#page-background > .stripes > .layer')[0];
 	var stripesVOffset = 0;
 	var repeatDur = 32; // seconds
-	var shiftVel = 4;
+	var shiftVel = 16;
 	
 	shiftStripes = function() {
 		stripesVOffset += shiftVel * repeatDur;
 		stripesLayer.setStyle({
-			'background-position-y': stripesVOffset+'px',
+			'background-position': '0 '+stripesVOffset+'px',
 		});
 		
 		setTimeout('shiftStripes();', repeatDur * 1000); // 32s, matching the “transition:” property on “#page-background > .stripes > .layer”
