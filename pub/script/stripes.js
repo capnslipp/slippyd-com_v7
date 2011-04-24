c66ec71d19ec0547fbf9e973aab5ae0343b7746b
Slippy Douglas <git@slippyd.com>
2011-04-23 23:33:06 -0400

    A little slower now...

--- pub/script/stripes.js
+++ pub/script/stripes.js
@@ -3,8 +3,8 @@ var shiftStripes = null;
 $(document).onReady(function() {
 	var stripesLayer = $$('#page-background > .stripes > .layer')[0];
 	var stripesVOffset = 0;
-	var repeatDur = 32; // seconds
-	var shiftVel = 16;
+	var repeatDur = 32; // in seconds, must match the “transition:” property on “#page-background > .stripes > .layer”
+	var shiftVel = 8;
 	
 	shiftStripes = function() {
 		stripesVOffset += shiftVel * repeatDur;
@@ -12,7 +12,7 @@ $(document).onReady(function() {
 			'background-position': '0 '+stripesVOffset+'px',
 		});
 		
-		setTimeout('shiftStripes();', repeatDur * 1000); // 32s, matching the “transition:” property on “#page-background > .stripes > .layer”
+		setTimeout('shiftStripes();', repeatDur * 1000);
 	};
 	
 	shiftStripes();
